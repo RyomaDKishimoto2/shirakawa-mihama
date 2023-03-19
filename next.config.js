8b1caa8c9050d928f75926f3d0ca0e3932abf682
ryomaD1225 <ryoma20151225@gmail.com>
2023-03-19 18:43:12 +0900

    remove target

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Render.com でのビルド時に必要な設定
-  target: 'serverless',
   // ビルド後のファイルを生成するディレクトリを指定
-  distDir: 'build',
+  distDir: 'out',
   // デフォルトの設定
   webpack(config) {
     return config;
