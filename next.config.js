6a33c124b5e60c6d62476a0f591d749384db9461
ryomaD1225 <ryoma20151225@gmail.com>
2023-03-19 18:40:16 +0900

    設定を追加

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  // Render.com でのビルド時に必要な設定
+  target: 'serverless',
+  // ビルド後のファイルを生成するディレクトリを指定
+  distDir: 'build',
+  // デフォルトの設定
+  webpack(config) {
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
