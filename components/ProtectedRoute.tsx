cbf1c9fd36b46d3c47981a9fb83c2d39760d5a25
ryomaD1225 <ryoma20151225@gmail.com>
2023-03-28 00:53:58 +0900

    auth設定

--- components/ProtectedRoute.tsx
+++ components/ProtectedRoute.tsx
@@ -1,13 +1,20 @@
 import { useRouter } from 'next/router';
 import React, { useEffect } from 'react';
 import { useAuth } from '../context/AuthContext';
+import type { Liff } from '@line/liff';
 
-const ProtectedRoute = ({ children }: { children: React.ReactNode }) => {
+const ProtectedRoute = ({
+  children,
+  liff,
+}: {
+  children: React.ReactNode;
+  liff: Liff | null;
+}) => {
   const router = useRouter();
   const { user } = useAuth();
 
   useEffect(() => {
-    if (!user.uid) {
+    if ((!user.userId && !liff) || (liff && !liff.isLoggedIn())) {
       router.push('/login');
     }
   }, [router, user]);
--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -4,13 +4,45 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
+  signInWithCustomToken,
 } from 'firebase/auth';
 import { auth } from '../config/firebase';
+import type { Liff } from '@line/liff';
+import { readUser, RoleType } from '@/lib/user';
 
 interface UserType {
-  email: string | null;
-  uid: string | null;
+  userId: string | null;
+  role: RoleType | null;
 }
+
+// liff関連のlocalStorageのキーのリストを取得
+const getLiffLocalStorageKeys = (prefix: string) => {
+  const keys = [];
+  for (var i = 0; i < localStorage.length; i++) {
+    const key = localStorage.key(i);
+    if (key?.indexOf(prefix) === 0) {
+      keys.push(key);
+    }
+  }
+  return keys;
+};
+// 期限切れのIDTokenをクリアする
+const clearExpiredIdToken = (liffId: string) => {
+  const keyPrefix = `LIFF_STORE:${liffId}:`;
+  const key = keyPrefix + 'decodedIDToken';
+  const decodedIDTokenString = localStorage.getItem(key);
+  if (!decodedIDTokenString) {
+    return;
+  }
+  const decodedIDToken = JSON.parse(decodedIDTokenString);
+  // 有効期限をチェック
+  if (new Date().getTime() > decodedIDToken.exp * 1000) {
+    const keys = getLiffLocalStorageKeys(keyPrefix);
+    keys.forEach(function (key) {
+      localStorage.removeItem(key);
+    });
+  }
+};
 
 const AuthContext = createContext({});
 
@@ -18,21 +50,29 @@ export const useAuth = () => useContext<any>(AuthContext);
 
 export const AuthContextProvider = ({
   children,
+  liff,
 }: {
   children: React.ReactNode;
+  liff: Liff | null;
 }) => {
-  const [user, setUser] = useState<UserType>({ email: null, uid: null });
+  const [user, setUser] = useState<UserType>({ userId: null, role: null });
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
       if (user) {
-        setUser({
-          email: user.email,
-          uid: user.uid,
-        });
+        const snap = await readUser({ uid: user.uid });
+        const userInfo = snap.data();
+        if (userInfo) {
+          setUser({
+            userId: userInfo.userId,
+            role: userInfo.role,
+          });
+        } else {
+          setUser({ userId: null, role: null });
+        }
       } else {
-        setUser({ email: null, uid: null });
+        setUser({ userId: null, role: null });
       }
     });
     setLoading(false);
@@ -49,12 +89,35 @@ export const AuthContextProvider = ({
   };
 
   const logOut = async () => {
-    setUser({ email: null, uid: null });
+    setUser({ userId: null, role: null });
     await signOut(auth);
   };
 
+  const liffSignUp = async () => {
+    if (!liff || !liff.id) {
+      return;
+    }
+    clearExpiredIdToken(liff.id);
+    await liff.init({ liffId: liff.id });
+    try {
+      const result = await fetch('/api/verify', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          idToken: liff.getIDToken(),
+        }),
+      });
+      const data = await result.json();
+      return await signInWithCustomToken(auth, data.token);
+    } catch (error: any) {
+      throw new Error(error.message);
+    }
+  };
+
   return (
-    <AuthContext.Provider value={{ user, signUp, logIn, logOut }}>
+    <AuthContext.Provider value={{ user, signUp, logIn, logOut, liffSignUp }}>
       {loading ? null : children}
     </AuthContext.Provider>
   );
