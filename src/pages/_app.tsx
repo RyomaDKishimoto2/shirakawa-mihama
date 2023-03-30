83fa34b13ff53e05fbc27e407dc5fe9eaa4c8107
ryomaD1225 <ryoma20151225@gmail.com>
2023-03-31 00:22:22 +0900

    baseUrl設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import Navbar from '../../components/Navbar';
+// import Navbar from '../../components/Navbar';
+import Navbar from 'components/Navbar';
 import { AuthContextProvider } from '../../context/AuthContext';
 import type { Liff } from '@line/liff';
 import { useEffect, useState } from 'react';
