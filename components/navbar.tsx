0ee7a949088037275e0e538fa467aa49a19c2089
ryomaD1225 <ryoma20151225@gmail.com>
2023-04-06 19:34:49 +0900

    未使用変数削除

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -7,25 +7,6 @@ import { Download } from './download';
 const Navbar = ({ children }: { children: React.ReactNode }) => {
   const { user, logOut } = useAuth();
   const router = useRouter();
-
-  const menuItems = [
-    {
-      id: 1,
-      name: 'Top',
-      link: '/',
-    },
-    {
-      id: 2,
-      name: 'ログイン',
-      link: '/login',
-    },
-    {
-      id: 3,
-      name: 'サインアップ',
-      link: '/signup',
-    },
-  ];
-
   const handleLogout = async () => {
     try {
       await logOut();
@@ -35,9 +16,98 @@ const Navbar = ({ children }: { children: React.ReactNode }) => {
     }
   };
 
+  function classNames(...classes: string[]) {
+    return classes.filter(Boolean).join(' ');
+  }
+
   return (
     <>
-      <header className='flex flex-wrap container mx-auto max-w-full items-center p-6 justify-between bg-white shadow-md sticky top-0 z-50'>
+      <div className='bg-white border-b-2'>
+        <div className='mx-auto max-w-7xl p-2 sm:px-6 lg:px-8'>
+          <div className='relative flex h-16 items-center justify-between'>
+            <div className='flex flex-1 items-center justify-center sm:items-stretch sm:justify-start'>
+              <div className='flex flex-shrink-0 items-center'>
+                <img
+                  className='block h-16 w-auto lg:hidden'
+                  src='/static/kamon.png'
+                  alt='Your Company'
+                />
+                <img
+                  className='hidden h-16 w-auto lg:block'
+                  src='/static/kamon.png'
+                  alt='SHIRAKAWA SHOTEN'
+                />
+                {user && user.role === RoleType.ADMIN && <Download />}
+              </div>
+              <div className='flex space-x-4 items-center'>
+                <>
+                  {!user || !user.userId ? (
+                    <Link href='/signup' legacyBehavior>
+                      <a
+                        className={classNames(
+                          router.asPath === '/signup'
+                            ? 'bg-gray-900 text-white'
+                            : 'text-gray-400 bg-slate-100 hover:text-white',
+                          'rounded-md px-4 py-3 text-lg font-medium'
+                        )}
+                      >
+                        サインアップ
+                      </a>
+                    </Link>
+                  ) : (
+                    <>
+                      <Link href='/dashboard' legacyBehavior>
+                        <a
+                          className={classNames(
+                            router.asPath === '/dashboard'
+                              ? 'bg-gray-900 text-white'
+                              : 'text-gray-400 bg-slate-100 hover:text-white',
+                            'rounded-md px-4 py-3 text-lg font-medium'
+                          )}
+                        >
+                          日報入力
+                        </a>
+                      </Link>
+                      <li className='my-3 md:my-0 items-center mr-4 md:inline-block block '>
+                        <Link href='/members' legacyBehavior>
+                          <a
+                            className={classNames(
+                              router.asPath === '/members'
+                                ? 'bg-gray-900 text-white'
+                                : 'text-gray-400 bg-slate-100 hover:text-white',
+                              'rounded-md px-4 py-3 text-lg font-medium'
+                            )}
+                          >
+                            スタッフ管理
+                          </a>
+                        </Link>
+                      </li>
+                    </>
+                  )}
+                </>
+              </div>
+            </div>
+            <div className='absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0'>
+              {router.asPath === '/signup' && (
+                <Link href='/login' legacyBehavior>
+                  <a className='text-gray-300  rounded-md px-3 py-2 text-lg font-medium'>
+                    ログイン <span aria-hidden='true'>&rarr;</span>
+                  </a>
+                </Link>
+              )}
+              {user && user.userId && (
+                <a
+                  onClick={handleLogout}
+                  className='text-gray-300  rounded-md px-3 py-2 text-lg font-medium'
+                >
+                  ログアウト <span aria-hidden='true'>&rarr;</span>
+                </a>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <header className='flex flex-wrap container mx-auto max-w-full items-center p-6 justify-between bg-white shadow-md sticky top-0 z-50'>
         <div className='flex items-center text-blue-900 hover:text-blue-800 cursor-pointer transition duration-150 '>
           {user && user.role === RoleType.ADMIN ? <Download /> : 'SHIRAKAWA'}
         </div>
@@ -89,7 +159,7 @@ const Navbar = ({ children }: { children: React.ReactNode }) => {
             </>
           </ul>
         </nav>
-      </header>
+      </header> */}
       {children}
     </>
   );
