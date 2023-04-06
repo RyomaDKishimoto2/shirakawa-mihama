1541cccbed06cae6054c94f0ded5459cef25b239
ryomaD1225 <ryoma20151225@gmail.com>
2023-04-06 19:35:53 +0900

    dashboardへ遷移

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,15 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '@/styles/Home.module.css';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 export default function Home() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push('/dashboard');
+  }, []);
+
   return (
     <>
       <Head>
