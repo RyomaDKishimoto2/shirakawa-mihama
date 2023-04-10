34f2e5293c6225fe5a6d7ffa51844c4f49be8e3d
ryomaD1225 <ryoma20151225@gmail.com>
2023-04-11 03:58:07 +0900

    staff add delete

--- src/lib/member.ts
+++ src/lib/member.ts
@@ -3,12 +3,17 @@ import {
   doc,
   setDoc,
   getDoc,
+  getDocs,
+  updateDoc,
   collection,
   DocumentReference,
+  CollectionReference,
 } from 'firebase/firestore';
-import { Member } from '../../features/sales/Repositories';
+import { CreateMemberInput, Member } from '../../features/sales/Repositories';
+import { HouryType } from '../../features/const';
 
 const FILED_NAME = 'staff';
+const INFO_FILED_NAME = 'STAFF_INFO';
 
 export const readMembers = async () => {
   const ref = doc(db, 'staff/member') as DocumentReference<{
@@ -17,9 +22,32 @@ export const readMembers = async () => {
   return await getDoc(ref);
 };
 
+export const readMembersByAdmin = async () => {
+  const col = collection(
+    db,
+    INFO_FILED_NAME
+  ) as CollectionReference<CreateMemberInput>;
+  return await getDocs(col);
+};
+
+export const createNewMember = async (param: CreateMemberInput) => {
+  const newMemberRef = doc(collection(db, INFO_FILED_NAME), `${param.name}`);
+  return await setDoc(newMemberRef, param);
+};
+
 export const createMember = async (members: Member[]) => {
   const newMemberRef = doc(collection(db, FILED_NAME), 'member');
   return await setDoc(newMemberRef, {
     members: members,
   });
+};
+
+export const deleteMember = async (name: string) => {
+  const newSalesRef = doc(collection(db, INFO_FILED_NAME), `${name}`);
+  return await updateDoc(newSalesRef, { isDeleted: true });
+};
+
+export const updateSalary = async (name: string, salary: HouryType) => {
+  const newSalesRef = doc(collection(db, INFO_FILED_NAME), `${name}`);
+  return await updateDoc(newSalesRef, { salary: salary });
 };
