2f79a7988185c7ab7cdd1c8e9da16cdc77cb3569
ryomaD1225 <ryoma20151225@gmail.com>
2023-06-09 16:03:34 +0900

    勤務時間修正

--- features/const.ts
+++ features/const.ts
@@ -20,7 +20,7 @@ export const STATUS = { offWork: '休み', working: '出勤' } as const;
 export type StatusType = (typeof STATUS)[keyof typeof STATUS];
 
 export const HOURS = [
-  13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+  12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
 ] as const;
 export type HourType = (typeof HOURS)[number];
 
