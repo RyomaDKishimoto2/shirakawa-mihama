28fd1aa3befe2020bbfe5053ccfe6a40de2bb68c
ryomaDkishimoto <ryoma20151225@gmail.com>
2023-11-24 17:26:30 +0900

    不要コード整理

--- features/const.ts
+++ features/const.ts
@@ -1,11 +1,14 @@
-export const MONTHS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] as const;
-export type MonthType = (typeof MONTHS)[number];
-export const DAYS = [...Array(31)].map((_, i) => i + 1);
-export type DaysType = (typeof DAYS)[number];
+export const tachikaraUid = 'ZCjq6Q1QmENqDYIJJRvB8x4HoR33';
 export const YEARS = [2023, 2024, 2025, 2026] as const;
-export type YearType = (typeof YEARS)[number];
+export const MONTHS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] as const;
+export const DAYS = [...Array(31)].map((_, i) => i + 1);
 export const weekItems = ['日', '月', '火', '水', '木', '金', '土'] as const;
-export type DayOfWeekType = (typeof weekItems)[number];
+export const STATUS = { offWork: '休み', working: '出勤' } as const;
+export const HOURLY = [900, 950, 1000, 1100, 1200, 1300, 1400, 1500] as const;
+export const MINUTES = [0, 15, 30, 45] as const;
+export const HOURS = [
+  12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+] as const;
 export const WEATHERS = [
   '晴れ',
   '曇り',
@@ -14,22 +17,28 @@ export const WEATHERS = [
   '雨のち晴',
   '晴れのち雨',
 ] as const;
-export type WeatherType = (typeof WEATHERS)[number];
-
-export const STATUS = { offWork: '休み', working: '出勤' } as const;
-export type StatusType = (typeof STATUS)[keyof typeof STATUS];
-
-export const HOURS = [
-  12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
+export const CHANGES = [
+  'Ichiman',
+  'Gosen',
+  'Nisen',
+  'Sen',
+  'Gohyaku',
+  'Hyaku',
+  'Gojyu',
+  'Jyu',
+  'Go',
+  'Ichi',
 ] as const;
+
+export type MonthType = (typeof MONTHS)[number];
+export type DaysType = (typeof DAYS)[number];
+export type YearType = (typeof YEARS)[number];
+export type DayOfWeekType = (typeof weekItems)[number];
+export type WeatherType = (typeof WEATHERS)[number];
+export type StatusType = (typeof STATUS)[keyof typeof STATUS];
 export type HourType = (typeof HOURS)[number];
-
-export const MINUTES = [0, 15, 30, 45] as const;
 export type MinuteType = (typeof MINUTES)[number];
-
-export const HOURLY = [900, 950, 1000, 1100, 1200, 1300, 1400, 1500] as const;
 export type HouryType = (typeof HOURLY)[number];
-
 export type MemberType = {
   name: string;
   status: StatusType;
@@ -45,18 +54,7 @@ export type OptionalType = {
   name: string;
   value: number;
 };
-export const CHANGES = [
-  'Ichiman',
-  'Gosen',
-  'Nisen',
-  'Sen',
-  'Gohyaku',
-  'Hyaku',
-  'Gojyu',
-  'Jyu',
-  'Go',
-  'Ichi',
-] as const;
+
 export type ChangeLabelType = (typeof CHANGES)[number];
 export type ChangeStateType = {
   Ichiman: number;
@@ -70,28 +68,6 @@ export type ChangeStateType = {
   Go: number;
   Ichi: number;
 };
-
-export const SUPPLIERS = [
-  'suehiro',
-  'sakihama',
-  'miyazato',
-  'ganaha',
-  'BEEFshin',
-  'zenoki',
-  'sunny',
-  'shopping',
-  'zappi',
-  'kemutou',
-  'gyoumu',
-  'furikomiFee',
-  'cardFee',
-  'eigyou',
-  'koutuhi',
-  'yachin',
-  'kounetuhi',
-  'tushinhi',
-  'miyagi',
-] as const;
 
 export type SupplierType = {
   suehiro: number;
@@ -134,5 +110,3 @@ export type SalesType = {
   optionals: OptionalType[] | [];
   fakeCash: number;
 };
-
-export const tachikaraUid = 'ZCjq6Q1QmENqDYIJJRvB8x4HoR33';
